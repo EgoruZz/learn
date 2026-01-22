aa49ddec81cdba27d5ab5c047ac5081086113b34
Egor S. Sergeev <egor.s.sergeev@icloud.com>
2026-01-22 15:51:57 +0300

    e

--- Dsa-notes/math-floder/number-theory/a-folder/a.cpp
+++ Dsa-notes/math-floder/number-theory/a-folder/a.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
-int gcd_substract(int a, int b) {
+
+int gcd_diff(int a, int b) {
     if (a > b) swap(a, b);
     while (b > 0) (a > b) ? a -= b : b -= a;
     return a;
-}
-
-int gcd_mod_rec(int a, int b) {
-    return (b == 0) ? gcd_mod_rec(b, a % b) : a;
 }
 
 int gcd_mod(int a, int b) {
@@ -17,35 +15,88 @@ int gcd_mod(int a, int b) {
     return a;
 }
 
+int gcd_mod_rec(int a, int b) {
+    return (b == 0) ? gcd_mod_rec(b, a % b) : a;
+}
+
+// Используйте эту версию, если компилятор GCC/Clang:
+unsigned int stein(unsigned int a, unsigned int b) {
+    if (a == 0) return b;
+    if (b == 0) return a;
+
+    int shift = __builtin_ctz(a | b);
+    a >>= __builtin_ctz(a);
+
+    do {
+        b >>= __builtin_ctz(b);
+        if (a > b) std::swap(a, b);
+        b -= a;
+    } while (b);
+
+    return a << shift;
+}
+
 int lcm(int a, int b) {
     return a / gcd_mod(a, b) * b;
 }
 
-pair<vector<int>, vector<int> > fact_sqrt(int a) {
-    vector<int> num((int) log2(a), 0), deg((int) log2(a), 0);
-    int x = 2, pos = 0;
-    while (a > 1) {
-        if (a % x == 0) num[pos] = x;
-        while (a % x == 0) {
-            deg[pos]++;
-            a /= x;
-        }
-        x++;
-        pos++;
+vector<int> divide(int a) {
+    vector<int> divs, sort;
+    for (int x = 1; x * x <= a; x++) {
+        if (a % x == 0) divs.push_back(x);
+        if (a % x == 0 && a / x != x) divs.push_back(a / x);
     }
-    return make_pair(num, deg);
+    for (int i = 0; i < (int) divs.size(); i += 2) sort.push_back(divs[i]);
+    for (int i = (int) divs.size() - 1; i >= 1; i -= 2) sort.push_back(divs[i]);
+    return sort;
+}
+
+pair<vector<int>, vector<int> > fact(int a) {
+    vector<int> primes((int) log2(a), 0), powers((int) log2(a), 0);
+    int x = 2, i = 0;
+    while (a > 1) {
+        for (; a % x == 0; a /= x, powers[i]++);
+        (powers[i] == 0) ? x++: primes[i++] = x++;
+    }
+    return make_pair((primes.resize(i), std::move(primes)), (powers.resize(i), std::move(powers)));
+}
+
+int mu(int n) {
+    auto [primes, powers] = fact(n);
+    for (auto ai : powers) if (ai >= 2) return 0;
+    return (primes.size() % 2 == 0) ? 1 : -1;
+}
+
+int fi(int n) {
+    int result = 0;
+    for (auto d : divide(n)) result += mu(d) * (n / d);
+    return result;
+}
+
+int d(int n, int k) {
+    vector<int> keys = divide(n);
+    unordered_map<int, vector<int> > info;
+    for (auto key : keys) info[key] = divide(key);
+
+    unordered_map<int, int> di;
+    for (auto key : keys) di[key] = 1;
+
+    while (k--) {
+        unordered_map<int, int> dj;
+        for (auto key : keys) {
+            for (auto div : info[key]) {
+                dj[key] += di[div];
+            }
+        }
+        di = dj;
+    }
+
+    return di[n];
 }
 
 signed main() {
     int a, b;
-    // a = 5; b = 32;
-    cin >> a;
+    // cin >> a >> b;
 
-    //cout << gcd_substract(a, b) << '\n';
-    //cout << gcd_mod(a, b) << '\n';
-    pair<vector<int>, vector<int> > r = fact_sqrt(a);
-    vector<int> v1 = r.first, v2 = r.second;
-    for (int i = 0; i < (int) v1.size(); i++) cout << v1[i] << " ";
-    cout << '\n';
-    for (int i = 0; i < (int) v2.size(); i++) cout << v2[i] << " ";
+    cout << d(12, 5) << endl;
 }
--- Dsa-notes/math-floder/number-theory/a-folder/a.py
+++ Dsa-notes/math-floder/number-theory/a-folder/a.py
@@ -1,4 +1,3 @@
-from math import *
-
-n = 1_000_000_000_000_000
-print(sqrt(n), n)
+print(100+1)
+a = 20
+print(1 + a)
